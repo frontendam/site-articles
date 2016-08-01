70e77ebb854bbf6b494f8f49fe7b007cb37fa5e0
luan <luan.carvalho@axmconsultoria.com>
2016-08-01 16:57:58 -0400

    Inicio da Implementação das categorias.

--- client/js/config.js
+++ client/js/config.js
@@ -3,6 +3,18 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,$url
             .state('home', {
                 url: '/home',
                 templateUrl: 'views/article/article.html'
+            })
+            .state('categories', {
+              url: '/categories',
+              templateUrl:  'views/categories/categories.html'
+            })
+            .state('categories-new', {
+              url: '/categories/new',
+              templateUrl: 'views/categories/new.html'
+            })
+            .state('categories-update', {
+              url: '/categories/update',
+              templateUrl: 'views/categories/update.html'
             });
         $urlRouterProvider.otherwise('home');
     }]);
